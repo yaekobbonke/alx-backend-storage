dfdce1e5472ca6ffe33f8441e55fabe4cfbcb1ed
yaekobbonke <yaekobbundure@gmail.com>
2023-10-17 23:17:38 +0300

    all

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+'''
+a Python function that lists all documents in a collection
+'''
+
+
+def list_all(mongo_collection):
+    '''returns documents in a collection'''
+    documents = []
+
+    cursor = mongo_collection.find({})
+    for document in cursor:
+        documents.append(document)
+        return documents
