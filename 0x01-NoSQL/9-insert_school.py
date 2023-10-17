eb116cf682ea5e12a4d407ca8ca98f6a2a4e890d
yaekobbonke <yaekobbundure@gmail.com>
2023-10-18 00:02:31 +0300

    insert

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+"""Python function that inserts a new document in a collection"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """inserts a new document in a collection"""
+    return mongo_collection.insert_one(kwargs).inserted_id
